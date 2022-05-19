b9960ffedb873b08a0317b973f3ba5ef7f786d7f
Kieu Huynh <Anhkieu303252@gmail.com>
2022-05-19 23:39:21 +0700

    css phần Sản phẩm 1/2/3

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 console.log("hello world!");
+
+const $ = document.querySelector.bind(document);
+
+// Action like
+let likeElement = $(".home-product-item__like");
+likeElement.onclick = function () {
+  if (this.classList.contains("home-product-item__like--liked")) {
+    this.classList.remove("home-product-item__like--liked");
+  } else {
+    this.classList.add("home-product-item__like--liked");
+  }
+};
